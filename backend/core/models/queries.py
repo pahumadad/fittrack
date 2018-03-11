772b9eb7e4f6d1fbd14f4664e3652655f29a8cb6
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2018-03-11 01:41:37 -0300

    Update queries to include the user_measurements table

--- backend/core/models/queries.py
+++ backend/core/models/queries.py
@@ -1,6 +1,7 @@
 from ..db.db import db
-from ..db.schema.users import Users as u
-from ..db.schema.measurements import Measurements as m
+from ..db.schema.users import Users
+from ..db.schema.measurements import Measurements
+from ..db.schema.user_measurements import UserMeasurements
 
 
 def delete_element(element):
@@ -13,17 +14,46 @@ def insert_element(element):
     db.session.commit()
 
 
+def insert_elements(elements):
+    db.session.bulk_save_objects(elements)
+    db.session.commit()
+
+
 def get_user_by_id(id):
-    return u.query.filter_by(id=id).first()
+    return Users.query.filter_by(id=id).first()
 
 
 def get_user_by_email(email):
-    return u.query.filter_by(email=email).first()
+    return Users.query.filter_by(email=email).first()
+
+
+def get_user_measurement(u_id, m_id):
+    return Measurements.query \
+        .join(UserMeasurements) \
+        .filter(Measurements.id == m_id) \
+        .join(Users) \
+        .filter(Users.id == u_id) \
+        .all()
+
+
+def get_user_measurements(user_id):
+    return Measurements.query \
+        .join(UserMeasurements) \
+        .join(Users) \
+        .filter(Users.id == user_id) \
+        .all()
 
 
 def get_measurement_by_id(m_id):
-    return m.query.filter_by(id=m_id).first()
+    return Measurements.query.filter_by(id=m_id).first()
 
 
 def get_measurement_by_name(name):
-    return m.query.filter_by(name=name).first()
+    return Measurements.query.filter_by(name=name).first()
+
+
+def delete_user_measurements(user_id, measurements):
+    stmt = UserMeasurements.__table__.delete() \
+        .where(UserMeasurements.user == user_id) \
+        .where(UserMeasurements.measurement.in_(measurements))
+    db.engine.execute(stmt)

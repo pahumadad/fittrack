83758ee8ee6ff6d4cf4f8d71c0516d4df285121a
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2018-04-15 13:40:03 -0300

    Add schema of measurements_tracker table

---
+++ backend/core/db/schema/measurements_tracker.py
@@ -0,0 +1,14 @@
+from ..db import db
+
+
+class MeasurementsTracker(db.Model):
+    __tablename__ = "measurements_tracker"
+    id = db.    Column(db.Integer, primary_key=True)
+    session = db.Column(db.Integer,
+                        db.ForeignKey("measurements_sessions.id"),
+                        nullable=False)
+    measurement = db.Column(db.Integer,
+                            db.ForeignKey("measurements.id"),
+                            nullable=False)
+    value = db.Column(db.Float)
+    conclusion = db.Column(db.String(120))
